df2aae4e536b9f49bd2fcf6a2fb810e5d148f971
Abdelsalam Habara <ahab0008@student.monash.edu>
2022-08-20 18:26:56 +1000

    Worked on API Call for Medicine Info

---
+++ prac_prescription_app/ClientApp/src/components/DrugInfo.js
@@ -0,0 +1,23 @@
+﻿import React, { useState } from 'react';
+
+// add parameter
+async function getDrug() {
+    let response = await fetch("https://api.fda.gov/drug/drugsfda.json?search=products.active_ingredients.name:" + "Levonorgestrel");
+    let data = await response.json();
+    let dataString = JSON.stringify(data);
+    console.table(data);
+    return JSON.stringify(data.results[0].products[0].brand_name);
+    //console.log(data.results["products"]);
+}
+
+const hello = () => {
+    return "hello world"
+}
+
+export function DrugInfo() {
+    getDrug()
+    return (
+        <div>
+            <h1> Hi my name is {getDrug} </h1>
+    </div>)
+} 
---
+++ prac_prescription_app/ClientApp/src/components/EmptyJSFile.js
@@ -0,0 +1,24 @@
+﻿import React, { useState } from 'react';
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+
+
+
+export function Example() {
+    const [percentage, setPercentage] = useState(0);
+
+    function updateProgress() {
+        if (percentage > 99) {
+            setPercentage(0);
+        }
+        else { setPercentage(percentage + 25); }
+    }
+
+    return (
+        <div>
+            <h1> hg</h1>
+            <CircularProgress value={percentage} />
+            <button onClick={() => updateProgress() }> Press Me </button>
+        </div>
+    );
+}
+
